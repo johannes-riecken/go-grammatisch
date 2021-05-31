a1cd550683e4162ed7a90f60999325e70a347938
Johannes Riecken <johannes.riecken@gmail.com>
2021-05-31 18:59:00 +0200

    Refactor for naked objects

--- pkg/go-grammatisch/model/astRegex.go
+++ pkg/go-grammatisch/model/astRegex.go
@@ -1,4 +1,3 @@
-// package model
 package model
 
 import (
--- pkg/go-grammatisch/server/routes.go
+++ pkg/go-grammatisch/server/routes.go
@@ -4,10 +4,50 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/johannes-riecken/go-grammatisch/pkg/go-grammatisch/model"
+	"log"
+	"net/http"
 )
 
 func AddRoutes(r *gin.Engine) {
+	globals := make(map[string]string)
 	r.GET("/index", getIndexHandler)
+	//r.Static("/static", "/Users/riecken/repos/go-grammatisch/pkg/templates/step00.gohtml")
+	r.LoadHTMLGlob("/Users/riecken/repos/go-grammatisch/pkg/templates/*.gohtml")
+	r.GET("/step00", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "step00.gohtml", globals)
+	})
+	r.POST("/step01", func(c *gin.Context) {
+		addPostFormToGlobals(c, globals)
+		log.Println(c.PostForm("grammars"))
+		c.HTML(http.StatusOK, "step01.gohtml", globals)
+	})
+	r.POST("/step02", func(c *gin.Context) {
+		addPostFormToGlobals(c, globals)
+		log.Println(c.PostForm("grammar"))
+		globals["astRegex"] = "" // TODO: Construct Grammar from string and call .ToRegex(). In the beginning, do it all with JSON
+		c.HTML(http.StatusOK, "step02.gohtml", globals)
+	})
+	r.POST("/step03", func(c *gin.Context) {
+		addPostFormToGlobals(c, globals)
+		log.Println(c.PostForm("astRegex"))
+		c.HTML(http.StatusOK, "step03.gohtml", globals)
+	})
+	r.POST("/step04", func(c *gin.Context) {
+		addPostFormToGlobals(c, globals)
+		log.Println(c.PostForm("inputDoc"))
+		c.HTML(http.StatusOK, "step04.gohtml", globals)
+	})
+	r.POST("/step05", func(c *gin.Context) {
+		addPostFormToGlobals(c, globals)
+		log.Println(c.PostForm("ast"))
+		c.Status(http.StatusOK)
+	})
+}
+
+func addPostFormToGlobals(c *gin.Context, globals map[string]string) {
+	for k, v := range c.Request.PostForm {
+		globals[k] = v[0]
+	}
 }
 
 func fakeUnmarshal(b []byte, grammar *model.Grammar) error {
